7f2e1d06b50d726dce3543c101aa07cb7b9c33e2
kageroZa <furkangumus@windowslive.com>
2023-09-22 12:20:42 +0300

    Update streamlit_app.py
    [Update] Added default to fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ fruits_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/f
 fruits_df = fruits_df.set_index('Fruit')
 
 # Let's put a picker so a user can pick the fruit they want
-streamlit.multiselect('Pick some fruits:', list(fruits_df.index))
+fruits_list = list(fruits_df.index)
+streamlit.multiselect('Pick some fruits:', fruits_list, fruits_list[:2])
 
 # display the table on the screen
 streamlit.dataframe(fruits_df)
